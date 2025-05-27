9a926654d900897809563513cd145384662b5d78
chihairou111 <chihairou@icloud.com>
2025-05-27 21:40:55 +0800

    Add Registration card and a little bit of registration form, add apple and google login (fake)

---
+++ src/registerpage.tsx
@@ -0,0 +1,11 @@
+import Registercard1 from "./registercard1";
+
+function RegisterPage() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gradient-to-br from-white via-[#f4f4f5] to-[#dbeafe]">
+      <Registercard1 />
+    </div>
+  );
+}
+
+export default RegisterPage;
